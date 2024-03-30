8d0af1d1272870ec2f32585dcc170ea7df2bb6c7
Martin Chovanec <chovanec.m11@gmail.com>
2024-03-30 22:58:54 +0100

    app created

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import { StudentList } from "./components/StudentList";
+import { StudentCreateForm } from "./components/StudentCreateForm";
+import { StudentDetail } from "./components/StudentDetail";
+import { StudentEditForm } from "./components/StudentEditForm";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <StudentList />,
+  },
+  {
+    path: "/students/:id",
+    element: <StudentDetail />,
+  },
+  {
+    path: "/students/:id/edit",
+    element: <StudentEditForm />,
+  },
+  {
+    path: "/students/create",
+    element: <StudentCreateForm />,
+  },
+]);
+
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
